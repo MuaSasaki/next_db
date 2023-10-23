d035bd58abf2f48080e5652ceb092870b9a2dcb0
MuaSasaki <muua.sasak@clementec.com>
2023-10-23 14:36:51 +0900

    バリテーション完了

--- src/pages/api/product/index.ts
+++ src/pages/api/product/index.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse, NextApiHandler } from 'next';
 import  prisma  from '../../../lib/Prisma';
+import { ProductGetType } from '@/types/productType';
 
 
 const getHandler = async (
@@ -23,6 +24,34 @@ const getHandler = async (
     res.status(statusCode).json(resStock)
 };
 
+// const putHandler = async (
+//     req:NextApiRequest,res:NextApiResponse
+// )=>{
+//     let statusCode = 200;
+//     const body:ProductGetType = req.body
+//     const resStock = await prisma.product
+//     .update({
+//         where:{
+//             pro_id:(body.id,)
+//         },
+//         data:{
+//             body.pro_name,
+//         }
+//     })
+//     .then((res) => {
+//         return res;
+//     })
+//     .catch((err) => {
+//         statusCode  = 500;
+//         console.log(err);
+//         return {error:'Failed to read'};
+//     })
+//     .finally(async () => {
+//         await prisma.$disconnect();
+//     })
+
+//     res.status(statusCode).json(resStock)
+// };
 
 const handler: NextApiHandler = (req,res) =>{
     
--- src/pages/product/[productId]/index.tsx
+++ src/pages/product/[productId]/index.tsx
@@ -4,7 +4,17 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { ProductGetType } from '@/types/productType';
 import Layout, { siteTitle } from '@/components/layout';
+import { useForm, SubmitHandler } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
+
+type FormValues = {
+  pro_name: string;
+  maker:string;
+  category:string;
+  price:number;
+};
 
 const dataProps:ProductGetType ={
   id : 0,
@@ -13,6 +23,13 @@ const dataProps:ProductGetType ={
   category:"",
   price:0,
 }
+
+const schema = yup.object().shape({
+  pro_name: yup.string().required("商品名は必須項目です。"),
+  maker: yup.string().required("メーカーは必須項目です。"),
+  category: yup.string().required("カテゴリーは必須項目です。"),
+  price: yup.number().positive().integer().typeError("数値の入力が必要です。").required("価格は必須項目です。"),
+})
 
 const ProductDetail:NextPage =() => {
   const [productData,setProductData] = useState<ProductGetType>(dataProps)
@@ -24,21 +41,32 @@ const ProductDetail:NextPage =() => {
       if (typeof product ===  "string")
       {
       setProductData(JSON.parse(product))
-      console.log("router.queryデータ→→→→→→→→",productData)
+      // console.log("router.queryデータ→→→→→→→→",productData)
       }else return;
     };
   },[router]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: {errors},
+  } = useForm<FormValues>({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit : SubmitHandler<FormValues> = async (data:FormValues) => {
+
+  }
 
   return(
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
+          <form onSubmit={handleSubmit(onSubmit)}>
+              <p>商品詳細編集</p>
         <table>
           <thead>
-            <tr>
-              <th>商品詳細編集</th>
-            </tr>
             <tr>
               <th>商品ID</th>
               <th>商品名</th>
@@ -50,10 +78,32 @@ const ProductDetail:NextPage =() => {
           <tbody>
             <tr>
               <td>{productData.id}</td>
+              <td>
+                <input {...register("pro_name")}/>
+                {errors["pro_name"] && errors["pro_name"]?.message}
+              </td>
+              <td>
+                <input {...register("maker")}/>
+                {errors["maker"] && errors["maker"]?.message}
+                </td>
+              <td>
+                <input {...register("category")}/>
+                {errors["category"] && errors["category"]?.message}
+              </td>
+              <td>
+                <input {...register("price")}/>
+                {errors["price"] && errors["price"]?.message}
+              </td>
             </tr>
           </tbody>
         </table>
         <input type="submit" />
+        </form>
+        {/* <p>{submitData.pro_name ?? submitData.pro_name}</p>
+        <p>{submitData.maker ?? submitData.maker}</p>
+        <p>{submitData.category ?? submitData.category}</p>
+        <p>{submitData.price ?? submitData.price}</p> */}
+        
     </Layout>
   );
 }
